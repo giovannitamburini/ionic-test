dc2b216bf7705abe78774e10c9d81c42c90663f4
giovannitamburini <giovatambu@gmail.com>
2024-04-07 14:22:46 +0200

    feat: added basic form to form page & changed the basic route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'offer-form',
     pathMatch: 'full',
   },
   {
--- src/app/pages/offer-form/offer-form.page.ts
+++ src/app/pages/offer-form/offer-form.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonSelect, IonSelectOption, IonDatetime, IonDatetimeButton, IonModal, IonButton} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-offer-form',
   templateUrl: './offer-form.page.html',
   styleUrls: ['./offer-form.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonInput, IonSelect,IonSelectOption, IonDatetime, IonDatetimeButton, IonModal, IonButton]
 })
 export class OfferFormPage implements OnInit {
 
