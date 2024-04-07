37a3a5bd2d4e21b2efb0c034acee274cbeca6ccc
giovannitamburini <giovatambu@gmail.com>
2024-04-07 16:53:34 +0200

    feat : added methods to convert date & to search offers (basic)

--- src/app/pages/offer-form/offer-form.page.ts
+++ src/app/pages/offer-form/offer-form.page.ts
@@ -14,13 +14,43 @@ export class OfferFormPage implements OnInit {
 
   formData = {
     location: '',
-    arrivalDate: '',
+    arrivalDate: new Date().toISOString(),
     stayDays: ''
   };
 
+  private baseUrl = 'https://test.appazzurroclub.it/serveracv/public/api/Api3/offers';
+
+  private finalQueryString = '&rooms%5B0%5D%5Bpeoples%5D=3&rooms%5B0%5D%5Broom_type_id%5D=37&lang=it';
+
   constructor() { }
 
+  
   ngOnInit() {
   }
+
+
+  // METHODS -------------------------------------------
+
+  // method to transform the date into the correct format(yyyy-mm-dd)
+  getOnlyDate(){
+
+    const formattedDate : string = this.formData.arrivalDate.split('T')[0];
+
+    this.formData.arrivalDate = formattedDate;
+
+  }
+
+  // method to search offers
+  searchOffers(){
+
+    this.getOnlyDate();
+
+   let customQueryString : string = '?hotel_code=' + this.formData.location + '&date_start=' + this.formData.arrivalDate + '&days=' + this.formData.stayDays;
+
+   let urlApi : string = this.baseUrl + customQueryString + this.finalQueryString;
+
+   console.log(urlApi);
+
+  };
 
 }
