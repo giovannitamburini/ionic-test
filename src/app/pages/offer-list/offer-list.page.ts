716228e0be7c8cbfd00021fe789f8c6c5708c530
giovannitamburini <giovatambu@gmail.com>
2024-04-08 02:50:39 +0200

    feat: added button to offer-list to return to the offer-form

--- src/app/pages/offer-list/offer-list.page.ts
+++ src/app/pages/offer-list/offer-list.page.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 import { OfferService } from 'src/app/service/offer.service';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-offer-list',
   templateUrl: './offer-list.page.html',
   styleUrls: ['./offer-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
 })
 export class OfferListPage implements OnInit {
 
   // dove salverò i dati ricevuti dalla ricerca
   data: any;
 
-  constructor(private offerService: OfferService ) { }
+  constructor(private navCtrl: NavController ,private offerService: OfferService ) { }
 
   ngOnInit() {
 
@@ -25,4 +26,10 @@ export class OfferListPage implements OnInit {
     console.log(this.data['data']);
   }
 
+
+  // metodo per tornare alla pagina del form
+  navigateToForm() {
+
+    this.navCtrl.navigateForward('/offer-form');
+  }
 }
