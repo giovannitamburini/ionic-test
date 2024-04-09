506910f408dee7b568eed70471851b8814226efb
giovannitamburini <giovatambu@gmail.com>
2024-04-09 11:02:55 +0200

    feat : added method to take device language &  added to the query string

--- src/app/pages/offer-form/offer-form.page.ts
+++ src/app/pages/offer-form/offer-form.page.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 import { OfferService } from 'src/app/service/offer.service';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonSelect, IonSelectOption, IonDatetime, IonDatetimeButton, IonModal, IonButton} from '@ionic/angular/standalone';
+import { Device } from '@capacitor/device';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonInput, IonSelect, IonSelectOption, IonDatetime, IonDatetimeButton, IonModal, IonButton, IonImg} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-offer-form',
   templateUrl: './offer-form.page.html',
   styleUrls: ['./offer-form.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonInput, IonSelect,IonSelectOption, IonDatetime, IonDatetimeButton, IonModal, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonInput, IonSelect,IonSelectOption, IonDatetime, IonDatetimeButton, IonModal, IonButton, IonImg]
 })
 export class OfferFormPage implements OnInit {
 
@@ -23,19 +24,47 @@ export class OfferFormPage implements OnInit {
   // variabili necessarie per fare la chiamata api--------
   private baseUrl = 'https://test.appazzurroclub.it/serveracv/public/api/Api3/offers';
 
-  private finalQueryString = '&rooms%5B0%5D%5Bpeoples%5D=3&rooms%5B0%5D%5Broom_type_id%5D=37&lang=it';
+  private finalQueryString = '&rooms%5B0%5D%5Bpeoples%5D=3&rooms%5B0%5D%5Broom_type_id%5D=37&lang=';
 
   private urlApi = '';
+
+  // lingue disponibili chiamata api
+  private allowedLanguages = ["it", "en", "de", "fr"];
+
+  // dove salvo la lingua del device con metodo getDeviceLang()
+  private deviceLang = '';
 
 
   constructor(private navCtrl: NavController, private offerService: OfferService) { }
 
   
   ngOnInit() {
+    this.getDeviceLang();
   }
 
 
   // METHODS -------------------------------------------
+
+  // metodo per ottenere 
+  async getDeviceLang() {
+
+    const lang = await Device.getLanguageCode();
+
+    // console.log(lang.value);
+
+    // this.deviceLang = lang.value;
+
+  if (!this.allowedLanguages.includes(lang.value)) {
+
+      this.deviceLang = "en";
+  } else {
+    
+      this.deviceLang = lang.value;
+
+      console.log(this.deviceLang);
+  }
+
+  }
 
   // method to transform the date into the correct format(yyyy-mm-dd)
   getOnlyDate(){
@@ -52,7 +81,7 @@ export class OfferFormPage implements OnInit {
     // compongo la query string
     let customQueryString : string = '?hotel_code=' + this.formData.location + '&date_start=' + this.formData.arrivalDate + '&days=' + this.formData.stayDays;
 
-    this.urlApi = this.baseUrl + customQueryString + this.finalQueryString;
+    this.urlApi = this.baseUrl + customQueryString + this.finalQueryString + this.deviceLang;
   }
 
   // metodo per attivare il bottone submit nel caso gli input siano tutti settati
